d669861c784d50aa99d650910278c713f770135a
Archita Racherla <archita.racherla@gmail.com>
2021-05-03 00:06:03 -0500

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
-/*fetch(
-  "https://app.ticketmaster.com/discovery/v2/events?apikey=5SeBGIy4GsBTA6ttR5Pu2buX8RhCTCsq&radius=25&size=200&unit=miles&locale=*&startDateTime=2021-04-27T18:06:00Z&endDateTime=2021-12-31T18:06:00Z&city=Austin"
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
 
-fetch(
-  "https://api.mapbox.com/directions/v5/mapbox/driving/-73.996%2C40.732%3B-73.991%2C40.735?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1IjoiYXdlZ2hvcnN0IiwiYSI6ImNrbnpianptdzAyYWkzMG85aG52NHY2YnYifQ.XX_caU54wujSY7FkrRplrA"
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-*/
 
 var eventID;
 function getUserInfo(){
@@ -51,12 +40,6 @@ console.log(genreSelection);
 
 
 
-/*var dates = document.getElementById("dates");
-var datesSelection = dates.options[dates.selectedIndex].value;
-if(datesSelection === "dateRange"){
-  $("#dateRange").datepicker();
-}
-*/
 
 var startDate = document.getElementById("datepicker-start").value;
 var endDate = document.getElementById("datepicker-end").value;
@@ -192,13 +175,14 @@ document.getElementById("displayEvents").addEventListener('click',function(e){
     var event_Name = ticketMasterResponse._embedded.events[eventID].name;
     var  event_Date= document.querySelector('.date' + eventID).textContent;
     console.log(document.getElementsByClassName('0'));
-    var lat = ticketMasterResponse._embedded.events[eventID]._embedded.venues[0].location.latitude;
-    var long = ticketMasterResponse._embedded.events[eventID]._embedded.venues[0].location.longitude;
+    var venue_Lat = ticketMasterResponse._embedded.events[eventID]._embedded.venues[0].location.latitude;
+    var venue_Long = ticketMasterResponse._embedded.events[eventID]._embedded.venues[0].location.longitude;
     
        console.log("Event Name " +event_Name);
        console.log("Date " +event_Date);
-       console.log("Lat " +lat);
-       console.log("Long" +long);
+       console.log("Lat " +venue_Lat);
+       console.log("Long" +venue_Long);
+       updateArray(event_Name, event_Date, venue_Lat, venue_Long);
        document.querySelector('#EventsPage').style.display = "none";
    }
 });
